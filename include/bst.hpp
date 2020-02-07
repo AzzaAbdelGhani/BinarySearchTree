9a5598b31aa8341349d1fb8129544a154244ccec
Nicolas <solonicolas@hotmail.it>
2020-02-07 16:03:46 +0100

    first make compilation

--- include/bst.hpp
+++ include/bst.hpp
@@ -7,63 +7,6 @@
 #include <iterator>
 #include <utility>
 
-template <typename k, typename v, typename c = std::less<k>>
-class bst{
-    c op;
-    std::unique_ptr<node> head;
-
-    public:
-        bst(c comp): op{comp} {};
-        bst(k key, v value, c comp): head{node(std::pair<k,v>(key,value))}, op{comp} {};
-
-        using node_type = node<std::pair<const k,v>>;
-        using pair_type = typename node_type::value_type;
-        using iterator = _iterator<node_type, pair_type>;
-        using const_iterator = _iterator<node_type, const pair_type>;
-
-        std::pair<iterator, bool> insert(const pair_type& x);
-        std::pair<iterator, bool> insert(pair_type&& x);
-
-        // TODO: what if we have an odd number of args?? Possibly exception!
-        // probably args should be pairs because in that way we can pass different types of k and v
-        // ask SARTORIIII
-
-        // TODO: if insert thorows an exception, should emplace throw it too?
-        template<class... Types>
-        std::pairs<iterator,bool> emplace(Types&&... args);
-
-        void clear() noexcept;
-
-        iterator begin();
-        const_iterator begin() const;
-        const_iterator cbegin() const; // TODO: maybe add cbegin () without const??
-
-        iterator end();
-        const_iterator end() const;
-        const_iterator cend() const;
-
-        iterator find(const k& x) noexcept;
-        const_iterator find(const k& x) const;
-
-        void balance();
-
-        v& operator[](const k& x);
-        v& operator[](k& x);
-
-        friend
-        std::ostream& operator<<(std::ostream& os, const bst& x);
-
-        // copy semantic
-        bst(const bst& b); // copy constr
-        bst& operator=(const bst& b); //copy assign
-
-        // move semantic
-        bst(const bst&& b) noexcept; // move constr
-        bst& operator=(const bst&& b) noexcept; //move assign
-
-        void erase(const k& x);
-};
-
 template <typename T>
 class node {
     T value;
@@ -75,7 +18,7 @@ class node {
     public:
         node(T p): value{p}, left{nullptr}, right{nullptr}, parent{nullptr} {};
         node(T p, node* n): value{p}, left{nullptr}, right{nullptr}, parent{n} {};
-        ~node(){delete parent; ~left; ~right;}; // TODO: delete or delete[] ??
+        ~node(){delete parent;}; // TODO: take care of shared pointers
         using value_type = T;
 
         //getters
@@ -91,7 +34,7 @@ class _iterator {
 
     public:
 
-        explicit _iterator(node* x)noexcept : current{x} {};
+        explicit _iterator(node_type* x)noexcept : current{x} {};
         using value_type = T;
         using reference = value_type&;
         using pointer = value_type*;
@@ -142,5 +85,65 @@ class _iterator {
             return !(a == b);
         }
 };
+
+template <typename k, typename v, typename c = std::less<k> >
+class bst{
+    using node_type = node<std::pair<const k,v> >;
+    c op;
+    std::unique_ptr<node_type> head;
+
+    public:
+        bst(c comp): op{comp} {};
+        bst(k key, v value, c comp): head{std::make_unique<node_type>(node_type(std::pair<k,v>(key,value)))}, op{comp} {};
+
+        
+        using pair_type = typename node_type::value_type;
+        using iterator = _iterator<node_type, pair_type>;
+        using const_iterator = _iterator<node_type, const pair_type>;
+
+        std::pair<iterator, bool> insert(const pair_type& x);
+        std::pair<iterator, bool> insert(pair_type&& x);
+
+        // TODO: what if we have an odd number of args?? Possibly exception!
+        // probably args should be pairs because in that way we can pass different types of k and v
+        // ask SARTORIIII
+
+        // TODO: if insert thorows an exception, should emplace throw it too?
+        template<class... Types>
+        std::pair<iterator,bool> emplace(Types&&... args);
+
+        void clear() noexcept;
+
+        iterator begin();
+        const_iterator begin() const;
+        const_iterator cbegin() const; // TODO: maybe add cbegin () without const??
+
+        iterator end();
+        const_iterator end() const;
+        const_iterator cend() const;
+
+        iterator find(const k& x) noexcept;
+        const_iterator find(const k& x) const;
+
+        void balance();
+
+        v& operator[](const k& x);
+        v& operator[](k& x);
+
+        friend
+        std::ostream& operator<<(std::ostream& os, const bst& x);
+
+        // copy semantic
+        bst(const bst& b); // copy constr
+        bst& operator=(const bst& b); //copy assign
+
+        // move semantic
+        bst(const bst&& b) noexcept; // move constr
+        bst& operator=(const bst&& b) noexcept; //move assign
+
+        void erase(const k& x);
+};
+
+
 
 #endif
---
+++ main.cc
@@ -0,0 +1,12 @@
+#include <bst.hpp>
+
+int main(){
+
+    bst<int, int, std::less<int>> t{1,1,std::less<int>()};
+    std::cout<< "ciao" << std::endl;
+    return 0;
+
+
+
+}
+

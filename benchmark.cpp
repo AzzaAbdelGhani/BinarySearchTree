8c1cfce73a52bb71e0cbf1d2fe37c1a3032a3a8e
earendil94 <francesco.brand94@gmail.com>
2020-02-14 16:32:37 +0100

    Updates to benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,6 +1,9 @@
 #include <bst.hpp>
 #include <map>
-#include<chrono>
+#include <chrono>
+#include <random>
+
+enum class method{ insert, find, emplace};
 
 int main(){
 
@@ -35,5 +38,105 @@ int main(){
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << " (ms)" << std::endl;  
 
 
+    std::cout << N << " finds on unbalanced bst" << std::endl;
+    begin = std::chrono::steady_clock::now();
+
+    for(unsigned int i = 0; i < N; ++i){
+        bst1.find(i);
+    }
+
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << " (ms)" << std::endl;  
+
+    std::cout << N << " finds on map" << std::endl;
+    begin = std::chrono::steady_clock::now();
+    for(unsigned int i = 0; i < N; ++i){
+        map1.find(i);
+    }
+
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << " (ms)" << std::endl;  
+
+
+    //Clear the trees
+    std::cout << "Clearing the trees" << std::endl;
+    bst1.clear();
+    map1.clear();
+
+
+
+
+
+    //Clear the trees
+    std::cout << "Clearing the trees" << std::endl;
+    bst1.clear();
+    map1.clear();
+
+
+    //Inserting random numbers
+
+    //Using the std uniform int distribution
+    std::random_device rd;  
+    std::mt19937 gen(rd()); 
+    std::uniform_int_distribution<> dis(1, N); //This way we might end up with duplicates numbers
+    //so insert should just not put them in the bst
+
+    std::cout << N << " random inserts bst" << std::endl;
+    begin = std::chrono::steady_clock::now();
+
+    for(unsigned int i = 0; i < N; ++i){
+        auto tmp = dis(gen);
+        bst1.insert(std::make_pair(tmp,tmp));
+    }
+
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << " (ms)" << std::endl;  
+
+
+    std::cout << N << " random inserts map" << std::endl;
+    begin = std::chrono::steady_clock::now();
+
+    for(unsigned int i = 0; i < N; ++i){
+        auto tmp = dis(gen);
+        map1.insert(std::make_pair(tmp,tmp));
+    }
+
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << " (ms)" << std::endl;  
+
+
+    //Random finds
+
+    std::cout << N << " random finds bst" << std::endl;
+    begin = std::chrono::steady_clock::now();
+
+    for(unsigned int i = 0; i < N; ++i){
+        auto tmp = dis(gen);
+        bst1.find(tmp);
+    }
+
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << " (ms)" << std::endl;  
+
+
+    std::cout << N << " random finds map" << std::endl;
+    begin = std::chrono::steady_clock::now();
+
+    for(unsigned int i = 0; i < N; ++i){
+        auto tmp = dis(gen);
+        map1.find(tmp);
+    }
+
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << " (ms)" << std::endl;  
+
+
+    //Clear the trees
+    std::cout << "Clearing the trees" << std::endl;
+    bst1.clear();
+    map1.clear();
+
 }
 
+// template<class T>
+// void run(T object, )
